d2eba63e5f4384d96cdfbdfc32b90521f72b7f20
Alisha <fexobe@gmail.com>
2020-07-29 12:20:59 -0700

    add header file

---
+++ header.html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Ace in the hole template</title>
+    <link rel = "stylesheet" type="text/css" href=
+    styles.php"/>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+  
+<body>
+           
+    <header class="header">
+        <ul class="menu">
+            <li>
+                <a href="#">Home</a>
+                <a href="#">Event Information</a>
+                <a href="#">Registration</a>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </header>
+</html>
