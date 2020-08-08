e917b2f14c76a751de1e70d26d806564fe30f385
Alisha <fexobe@gmail.com>
2020-08-08 15:50:38 -0700

    register

--- register.html.php
+++ register.html.php
@@ -5,38 +5,28 @@
 
           <form class="registration" action="register_form.php" method="post">
 
-            <label for="rname">Name</label>
-            <input type="text" id="name" nam="name" placeholder="Full name.."><br>
+            <input type="text" id="name" name="name" placeholder="Full name.."><br><br>
 
-            <label for="re-mail">E-mail</label>
-            <input type="text" id="email" name="email" placeholder="E-mail.."><br>
+            <input type="text" id="email" name="email" placeholder="E-mail.."><br><br>
 
-            <label for="rphone">Phone Number</label>
-            <input type="text" id="phone" name="phone" placeholder="Phone Number.."><br>
+            <input type="text" id="phone" name="phone" placeholder="Phone Number.."><br><br>
 
-            <label for="rhouse">House number</label>
-            <input type="text" id="house" name="house" placeholder="House number.."><br>
+            <input type="text" id="house" name="house" placeholder="House number.."><br><br>
 
-            <label for="rstreet">Street</label>
-            <input type="text" id="street" name="street" placeholder="Street Name.."><br>
+            <input type="text" id="street" name="street" placeholder="Street Name.."><br><br>
 
-            <label for="rcity">City</label>
-            <input type="text" id="city" name="city" placeholder="Name of city.."><br>
+            <input type="text" id="city" name="city" placeholder="Name of city.."><br><br>
 
-            <label for="rstate">State</label>
-            <input type="text" id="state" name="state" placeholder="Name of State.."><br>
+            <input type="text" id="state" name="state" placeholder="Name of State.."><br><br>
 
-            <label for="rzip">Zip code</label>
-            <input type="text" id="zip" name="zip" placeholder="Zip code.."><br>
+            <input type="text" id="zip" name="zip" placeholder="Zip code.."><br><br>
 
-            <label for="revent">Event</label>
-            <input type="text" id="event" name="event" placeholder="Event name.."><br>
+            <input type="text" id="event" name="event" placeholder="Event name.."><br><br><br>
 
             <Button type="submit" name="submit">Submit form</Button>
-            
-
+            <br>
+      
           </form>
         </main>
         <?php include 'footer.html.php';?>
     </body>
-</html>
