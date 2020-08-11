effcfb784d68a571c85daea4a52f3210a272ae99
Alisha <fexobe@gmail.com>
2020-08-10 18:29:40 -0700

    added role

--- contact.html.php
+++ contact.html.php
@@ -13,6 +13,16 @@
             <br>
             <br>
             
+            <label for="role">*Role:</label><br><br>
+          <select size="1" name="role" id="role">
+            <option>Select an role</option>
+            <option value="athlete">Athlete</option>
+            <option value="volunteer">Volunteer</option>
+         </select>
+            <br>
+            <br>
+            <br>    
+
             <label for="questions">*Questions and Comments:</label><br>
             <textarea id="questions" name="questions">
             
@@ -37,9 +47,11 @@
             if (isset($_POST['submit'])) {
                 $name = $_POST['name'];
                 $email = $_POST['email'];
+                $role = $_POST['role'];
                 $questions = $_POST['questions'];
+                
 
-            $sql = "INSERT INTO contact (name, email, questions) VALUES ('{$name}', '{$email}', '{$questions}')";
+            $sql = "INSERT INTO contact (name, email, role, questions) VALUES ('{$name}', '{$email}', '{$role}', '{$questions}')";
 
             require_once 'dbconfig.php';
 
