1a15626d552b05aa9453f82a252f399dbed5779e
Alisha <fexobe@gmail.com>
2020-08-20 16:46:34 -0700

    revised social and slideshow

--- template.html.php
+++ template.html.php
@@ -7,38 +7,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+
 </head>
 
 <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0" nonce="IvsG3Z6q"></script>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0" nonce="5ynWOowV"></script>
+
+
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
 <body>
+
   <main class="home">
     
-      <!-- Slideshow container -->
-      <div class="slideshow-container">
+<div class="slideshow-container">
 
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="images/ace15.jpg" style="width:100%">
-  <div class="text">All abilities welcome</div>
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="images/ace15.jpg" alt="all abilities" style="width:100%">
+    <div class="text">All abilities are welcome</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="images/ace17.jpg"  alt="people running" style="width:100%">
+    <div class="text">Marathons</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="images/ace6.jpg" alt="triathlon" style="width:100%">
+    <div class="text">Triathlon</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
 </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="images/ace17.jpg" style="width:100%">
-  <div class="text">Running</div>
-</div>
 
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="images/ace6.jpg" style="width:100%">
-  <div class="text">Triathlon</div>
-}
-</script>
 
-                <br>
-                
-        </div>   
 
             <div class="about">
 
@@ -56,7 +75,7 @@
             <div class="leftcol">
                 <h3>Weather Updates</h3>
 
-                <a href="https://www.weatherwx.com/forecast/or/portland.html" style="width:10px" target="_blank"><img src="https://www.weatherwx.com/forecast.php?forecast=zandh&place=portland&state=or&country=us&config=png&alt=hwizonenew" border="0" alt="Click for the latest Portland weather forecast."></a>
+                <a href="https://www.weatherwx.com/forecast/or/portland.html" style="width:10px" target="_blank"><img src="https://www.weatherwx.com/forecast.php?forecast=zandh&place=portland&state=or&country=us&config=png&alt=hwizonenew" alt="Click for the latest Portland weather forecast."></a>
 
             </div>
 
@@ -65,12 +84,46 @@
             <h3>Social Media Feeds</h3>
 
             <div class="fb-page" data-href="https://www.facebook.com/https://https://www.facebook.com/Cas222Aceinthehole-110661963841617&quot;" data-tabs="timeline" data-width="300" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/https://https://www.facebook.com/Cas222Aceinthehole-110661963841617&quot;" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/https://https://www.facebook.com/Cas222Aceinthehole-110661963841617&quot;">Cas222Aceinthehole</a></blockquote></div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
             <a class="twitter-timeline" data-width="300" data-height="400" href="https://twitter.com/Cas222Pcc?ref_src=twsrc%5Etfw">Tweets by Cas222Pcc</a> 
 
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    
+           
                      
         </div>
+
+        
+<script>
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+ </script>
     </main>
   </body>
 </html>
